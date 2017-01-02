2a1d8e9e31cc6b07803aa0f933e783cbc1ade0a3
ShirleyL <shulin_liu@foxmail.com>
2017-01-02 10:22:55 +0800

    modify the use of dictionaries

--- Practise&Test/Dictionares.py
+++ Practise&Test/Dictionares.py
@@ -26,6 +26,9 @@ names = ['csve','seve','jackie','seve','jackie','seve']
 for name in names:
     count[name] = count.get(name,0) + 1
 print count
+# dictionary traceback
+print 'csve' in count#true 
+print 'heju' in count#false
 
 # counting pattern
 counts = dict()
@@ -38,3 +41,34 @@ for word in words:
     counts[word] = counts.get(word,0) + 1
 print 'Counting: '
 print counts
+
+# get key, value, and items
+friends = {'Jack':24, 'Lucy':12, 'Bill':121}
+print friends.keys()
+print friends.values()
+print friends.items()
+for a,b in friends.items():
+    print a,b
+    
+# counting pattern in word.txt
+#import os
+#path = os.getcwd()
+#print path
+name = raw_input('Enter the file: ')
+#name = path + '/' + name
+handle = open(name)
+
+text = handle.read()
+words = text.split()
+
+counts = dict()
+for word in words:
+    counts[word] = counts.get(word,0) + 1
+
+bigcount = None
+bigword = None
+for word,count in counts:
+    if bigcount is None or count > bigcount:
+        bigword = word
+        bigcount = count
+print bigword,bigcount
