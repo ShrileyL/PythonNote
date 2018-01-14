d3a3156aa8079530dabb8ccdc24526fc3b13d1cc
ShirleyL <shulin_liu@foxmail.com>
2018-01-14 20:20:19 +0800

    add bagels

---
+++ Games/dragon.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  1 16:25:02 2018
+
+@author: ShirleyLiu
+"""
+
+def displayIntro():
+    print('''You are in a land full of dragons. In front of you,
+you see two caves. In one cave, the dragon is friendly
+and will share his treasure with you. The other dragon
+is greedy and hungry, and will eat you on sight.''')
+    print()
+
+def chooseCave():
+    cave
+    
+
+def checkCave(chosenCave):
+    
+    
+
